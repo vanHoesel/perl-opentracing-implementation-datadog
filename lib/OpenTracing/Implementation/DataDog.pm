abf5f562c5e4070c1b779feb7b609dff4e1b593d
Theo van Hoesel <tvanhoesel@perceptyx.com>
2020-01-15 15:40:31 +0100

    rename to `bootstrap_tracer`

--- lib/OpenTracing/Implementation/DataDog.pm
+++ lib/OpenTracing/Implementation/DataDog.pm
@@ -1,6 +1,8 @@
 package OpenTracing::Implementation::DataDog;
 
 =head1 NAME
+use strict;
+use warnings;
 
 OpenTracing::Implementation::DataDog - A Global Tracer Implentation
 
@@ -11,13 +13,21 @@ OpenTracing::Implementation::DataDog - A Global Tracer Implentation
 use aliased 'OpenTracing::Implementation::DataDog::Tracer';
 
 
-sub bootstrap {
+sub bootstrap_tracer {
     my $implementation_class = shift;
     
     my @implementation_args  = @_;
     
     return Tracer->new( @implementation_args );
 }
+
+
+
+BEGIN {
+    use Role::Tiny::With;
+    with 'OpenTracing::Implementation::Interface::Bootstrap'
+        if $ENV{OPENTRACING_INTERFACE}
+} # check at compile time, perl -c will work
 
 
 

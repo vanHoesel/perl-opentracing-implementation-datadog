ceb0e6247419f5d9f6267bec29ccc5553ccfa1aa
Theo van Hoesel <tvanhoesel@perceptyx.com>
2020-02-12 22:13:56 +0100

    release version v0.02

--- lib/OpenTracing/Implementation/DataDog.pm
+++ lib/OpenTracing/Implementation/DataDog.pm
@@ -3,7 +3,7 @@ package OpenTracing::Implementation::DataDog;
 use strict;
 use warnings;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use aliased 'OpenTracing::Implementation::DataDog::Tracer';
 

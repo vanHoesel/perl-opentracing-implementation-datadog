d95287077ea73c557812f00ace79aa1e0733045e
Theo van Hoesel <tvanhoesel@perceptyx.com>
2020-05-21 12:12:48 +0200

    Remove `on_close`, part of `OT::Roles` now

--- lib/OpenTracing/Implementation/DataDog/Scope.pm
+++ lib/OpenTracing/Implementation/DataDog/Scope.pm
@@ -14,27 +14,8 @@ our $VERSION = 'v0.30.1';
 
 use Moo;
 
-with 'OpenTracing::Role::Scope';
-
-use Types::Standard qw/CodeRef/;
-
-
-
-has on_close => (
-    is              => 'ro',
-    isa             => CodeRef,
-    default         => sub { sub { } },
-);
-
-
-
-sub close {
-    my $self = shift;
-    
-    return $self->on_close->( $self, @_ )
-    
+BEGIN {
+    with 'OpenTracing::Role::Scope';
 }
-
-
 
 1;

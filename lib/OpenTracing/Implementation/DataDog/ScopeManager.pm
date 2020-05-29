aefaba786cc4a503a474669cc1a4bbb2ecb14e92
Theo van Hoesel <tvanhoesel@perceptyx.com>
2020-05-29 17:31:05 +0200

    Simplify the `build_scope` with expected named params

--- lib/OpenTracing/Implementation/DataDog/ScopeManager.pm
+++ lib/OpenTracing/Implementation/DataDog/ScopeManager.pm
@@ -16,35 +16,17 @@ use Moo;
 
 use OpenTracing::Implementation::DataDog::Scope;
 
-use OpenTracing::Types qw/Scope/;
-use Types::Standard qw/Maybe/;
 
 
-
-has '+scope_builder' => (
-    lazy => 1,
-    builder => sub { shift->datadog_scope_builder->(@_) },
-);
-
-
-
-sub datadog_scope_builder {
+sub build_scope {
     my $self = shift;
-    my $span = shift;
     my $options = { @_ };
-    
-    # remove the `finish_span_on_close` option, which is for this method only! 
-    my $finish_span_on_close = 
-        exists( $options->{ finish_span_on_close } ) ?
-            !! delete $options->{ finish_span_on_close }
-            : !undef
-    ; # use 'truthness' of param if provided, or set to 'true' otherwise
     
     my $current_scope = $self->get_active_scope;
     
     my $scope = OpenTracing::Implementation::DataDog::Scope->new(
-        span                 => $span,
-        finish_span_on_close => $finish_span_on_close,
+        span                 => $options->{ span },
+        finish_span_on_close => $options->{ finish_span_on_close },
         on_close             => sub {
             $self->set_active_scope( $current_scope );
         }

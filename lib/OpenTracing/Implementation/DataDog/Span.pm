6f859d516a1c9a41647cd64e284542c032e0090e
Theo van Hoesel <tvanhoesel@perceptyx.com>
2020-06-08 23:19:20 +0200

    Add coercion for `+context`

--- lib/OpenTracing/Implementation/DataDog/Span.pm
+++ lib/OpenTracing/Implementation/DataDog/Span.pm
@@ -10,6 +10,11 @@ with 'OpenTracing::Role::Span';
 
 use OpenTracing::Implementation::DataDog::Utils qw/random_64bit_int/;
 
+use aliased 'OpenTracing::Implementation::DataDog::SpanContext';
+
+use Types::Standard qw/Str/;
+use Ref::Util qw/is_plain_hashref/;
+use Carp;
 has '+operation_name' => (
     isa => Str->where( 'length($_) <= 100' ),
 );
@@ -19,6 +24,22 @@ has '+operation_name' => (
 has '+span_id' => (
     default => sub{ random_64bit_int() }
 );
+
+
+
+has '+context' => (
+    coerce
+    => sub { is_plain_hashref $_[0] ? SpanContext->new( %{$_[0]} ) : $_[0] },
+    default
+    => sub { croak "Can not construct a default SpanContext" },
+);
+
+# OpenTracing does not provide any public method to instantiate a SpanContext.
+# But rootspans do need to have a context which comes from
+# the `$TRACER->extract_context` call, or it returns `undef` if there was no
+# such context.
+# Passing in a plain hash reference instead of a SpanContext will
+# instantiate such context with a 'fresh' `trace_id`
 
 
 

f123c544afb451d1b7df2d342030a6f66887969c
Theo van Hoesel <tvanhoesel@perceptyx.com>
2023-04-24 22:47:22 +0200

    Use `Math::BigInt` instead of `DataDog::ID`

--- lib/OpenTracing/Implementation/DataDog/Client.pm
+++ lib/OpenTracing/Implementation/DataDog/Client.pm
@@ -231,7 +231,7 @@ has _json_encoder => (
 );
 
 sub _build__json_encoder {
-    JSON()->new->utf8->canonical->pretty->convert_blessed()
+    JSON()->new->utf8->canonical->pretty->allow_bignum
 }
 #
 # I just love readable and consistant JSONs
